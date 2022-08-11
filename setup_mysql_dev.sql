4f4ce330f958f52981a542c464f70b8bbe4cee7b
yamilavila <git@github.com>
2022-08-11 17:13:02 +0000

    Task 3 debuging

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,9 @@
 --Script that prepares a MySQL server for the project:
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 --Creating user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 --Granting privileges
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 --Selecting
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
