74128cd27ea713833495651d809c01eaf203838c
Xavier <xavierperez1599@gmail.com>
2022-08-11 19:12:06 -0400

    adding user

--- models/user.py
+++ models/user.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-import os
-from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """This class defines a user by various attributes."""
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        places = relationship("Place", backref='user', cascade="delete")
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")
