07949c7b8fc73a249c307e8bea684a5cad1f92bb
yamilavila <git@github.com>
2022-08-12 00:39:47 +0000

    Task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from os import getenv
 
