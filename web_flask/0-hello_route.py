564558addff4e93236c3704e483a3123f84923c9
yamilavila <yamilpra@hotmail.com>
2022-09-11 04:59:42 -0400

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 '''Script that start Flask app'''
 from flask import Flask
-app = Flask('app')
+app = Flask(__name__)
 
 
 @app.route('/frontPage/', strict_slashes=False)
 def open_hbnb():
-    """print hello"""
+    """print Hello HBNB!"""
     return "Hello HBNB!"
 
-if __name__ '__main__':
+if __name__ == '__main__':
     """runs the app"""
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
