b76aeb311ce0b62f27601e2213312f815798b008
yamilavila <yamilpra@hotmail.com>
2022-09-11 05:40:06 -0400

    Task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Script in which starts a Flask app"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def open_hbnb():
+    """print Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """prints Hbnb"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    """runs the app"""
+    app.run(host='0.0.0.0', port=5000)
