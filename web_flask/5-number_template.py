2bc826866192be797e9a22b47199311174bb7851
yamilavila <yamilpra@hotmail.com>
2022-09-11 21:22:40 -0400

    Add file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,7 +42,7 @@ def num(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def num_template(n):
     """displays a HTML page only if n is a int"""
-    return render_template('5-number.html', num=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
